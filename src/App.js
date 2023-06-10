c8ad60183783d4334b0f69b251839134dc8a3bd0
roodhouse <rughjm@gmail.com>
2023-06-10 11:43:51 -0500

    adding fonts etc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>frontend</h1>
+      <h1 className="text-3xl font-bold underline font-['Fraunces']">
+      Hello world!
+    </h1>
+    <p className="font-['Manrope']">hi there</p>
     </div>
   );
 }
