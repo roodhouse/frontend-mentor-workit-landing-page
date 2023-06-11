60bb5227133ed2e54b6ab973676b96e6caeb7863
roodhouse <rughjm@gmail.com>
2023-06-11 13:50:47 -0500

    finished tablet adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ import Instagram from '../images/icon-instagram.svg'
 
 function Footer() {
   return (
-    <div id='footerContainer' className='pt-[74px] flex flex-col justify-center items-center px-4 mb-16'>
-        <div id='darkLogo' className='mb-[58px]'>
+    <div id='footerContainer' className='pt-[74px] flex flex-col justify-center items-center px-4 mb-16 lg:pt-0'>
+        <div id='darkLogo' className='mb-[58px] lg:flex lg:items-center lg:justify-center lg:max-w-[116px] lg:w-[116px]'>
             <img src={DarkLogo} alt="the darker logo" />
         </div>
-        <div id='socialMedia' className='flex flex-row justify-evenly w-full'>
+        <div id='socialMedia' className='flex flex-row justify-evenly w-full lg:max-w-[116px] lg:justify-between'>
             <div id='facebook'>
                 <img src={Facebook} alt="facebook" />
             </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,18 +3,18 @@ import ProfilePic from '../images/image-founder.webp'
 
 function Profile() {
   return (
-    <div id='profileSection' className='pt-5 px-4'>
-        <div id='profileImage'>
+    <div id='profileSection' className='pt-5 px-4 lg:px-10'>
+        <div id='profileImage' className='lg:w-72 lg:mb-[-65px]'>
             <img src={ProfilePic} alt='the man' />
         </div>
-        <div id='profileMeat' className='flex flex-col justify-center items-center text-center bg-darkPurple relative bottom-16'>
-        <div id='profileHeading' className='text-white p-8 font-["Fraunces"] text-[32px] leading-[48px] font-semibold'>
+        <div id='profileMeat' className='flex flex-col justify-center items-center text-center bg-darkPurple relative bottom-16 lg:w-[514px] lg:ml-[174px] lg:items-start lg:pl-8'>
+        <div id='profileHeading' className='text-white p-8 font-["Fraunces"] text-[32px] leading-[48px] font-semibold lg:text-5xl lg:leading-[56px] lg:pb-6 lg:pl-0'>
             <h2>Be the first to test</h2>
         </div>
-        <div id='profileDetails' className='text-white p-8 font-["Manrope"] text-base leading-7 text-center font-normal'>
-            <p>Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I'll be in touch to schedule a call.</p>
+        <div id='profileDetails' className='text-white p-8 font-["Manrope"] text-base leading-7 text-center font-normal lg:pt-0 lg:pb-6 lg:pl-0'>
+            <p className='lg:text-left'>Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I'll be in touch to schedule a call.</p>
         </div>
-        <div id='profileButton' className='text-darkPurple mb-8'>
+        <div id='profileButton' className='text-darkPurple mb-8 lg:mb-14'>
         <button className='text-darkPurple font-["Manrope"] font-bold text-base leading-8 text-center tracking-[-0.16px] bg-eucaplyptus px-6 py-3'>Apply for access</button>
         </div>
         </div>
